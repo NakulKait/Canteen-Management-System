1f7c7f2d9b3c267b1aba906cb50018417998bc17
Om Bongulwar <obongulwar@gmail.com>
2025-08-06 13:11:04 +0530

    Added OTP verification with expiry and cleanup logic

--- Backend/canteen-backend/src/main/java/com/canteen/backend/model/User.java
+++ Backend/canteen-backend/src/main/java/com/canteen/backend/model/User.java
@@ -1,47 +1,46 @@
-package com.canteen.backend.model;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import org.springframework.data.annotation.Id;
-import org.springframework.data.annotation.Transient;
-import org.springframework.data.mongodb.core.mapping.DBRef;
-import org.springframework.data.mongodb.core.mapping.Document;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
-import lombok.Data;
-
-@Data
-@Document(collection = "users")
-public class User {
-   
-	@Transient
-	public static final String SEQUENCE_NAME = "users_sequence";
+	package com.canteen.backend.model;
 	
-    @Id
-    private Long id;  // MongoDB uses String ObjectId by default
-    private String fullName;
-    private String email;
-    private String role;
-    private String password;
-    private boolean verified = false;
-    
-    @JsonIgnore
-    @DBRef(lazy=true)
-    private List<Orders>orderList=new ArrayList<>();
-    
-    
-    public void addOrder(Orders order)
-    {
-    	orderList.add(order);
-    	order.setMyUser(this);
-    }
-    
-    public void deleteOrder(Orders order)
-    {
-    	orderList.remove(order);
-    	order.setMyUser(null);
-    }
+	import java.time.LocalDateTime;
+	import java.util.ArrayList;
+	import java.util.List;
+	
+	import org.springframework.data.annotation.Id;
+	import org.springframework.data.annotation.Transient;
+	import org.springframework.data.mongodb.core.mapping.DBRef;
+	import org.springframework.data.mongodb.core.mapping.Document;
+	
+	import com.fasterxml.jackson.annotation.JsonIgnore;
+	
+	import lombok.Data;
+	
+	@Data
+	@Document(collection = "users")
+	public class User {
+	   
+	    @Transient
+	    public static final String SEQUENCE_NAME = "users_sequence";
+	    
+	    @Id
+	    private Long id;  // MongoDB uses String ObjectId by default
+	    private String fullName;
+	    private String email;
+	    private String role;
+	    private String password;
+	    private boolean verified = false;
+	
 
-}
+	
+	    @JsonIgnore
+	    @DBRef(lazy = true)
+	    private List<Orders> orderList = new ArrayList<>();
+	
+	    public void addOrder(Orders order) {
+	        orderList.add(order);
+	        order.setMyUser(this);
+	    }
+	
+	    public void deleteOrder(Orders order) {
+	        orderList.remove(order);
+	        order.setMyUser(null);
+	    }
+	}
