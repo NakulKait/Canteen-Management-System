4df0ceda35d372cca98daad2bd8f6d560f648dc2
NakulKait <nakulkait@gmail.com>
2025-08-09 21:19:08 +0530

    Permitted all endpoints to access

--- Backend/canteen-backend/src/main/java/com/canteen/backend/config/SecurityConfig.java
+++ Backend/canteen-backend/src/main/java/com/canteen/backend/config/SecurityConfig.java
@@ -21,15 +21,16 @@ public class SecurityConfig {
 	        http
 	            .csrf(csrf -> csrf.disable()) // Disable CSRF for Postman testing
 	            .authorizeHttpRequests(auth -> auth
-	            		.requestMatchers(HttpMethod.POST, "/register", "/login","/verify-otp").permitAll()
-	            		.requestMatchers(HttpMethod.GET, "/").permitAll()
-	            		 .requestMatchers(
-	                             "/v3/api-docs/**",
-	                             "/swagger-ui/**",
-	                             "/swagger-ui.html"
-	                         ).permitAll()
+//	            		.requestMatchers(HttpMethod.POST, "/register", "/login","/verify-otp").permitAll()
+//	            		.requestMatchers(HttpMethod.GET, "/").permitAll()
+//	            		 .requestMatchers(
+//	                             "/v3/api-docs/**",
+//	                             "/swagger-ui/**",
+//	                             "/swagger-ui.html"
+//	                         ).permitAll()
 
-	                .anyRequest().authenticated() // Everything else needs login
+	                //.anyRequest().authenticated() // Everything else needs login
+	            		.anyRequest().permitAll()
 	            );
 	        return http.build();
 	    }
